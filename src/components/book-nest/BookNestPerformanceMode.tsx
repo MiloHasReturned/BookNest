211be896be07dc645dd2bd847fa5ba618391bdcc
Maxi Gajda <maxigajda@gmail.com>
2026-05-22 08:31:24 +1000

    Stabilize backdrop during scrolling

--- src/components/book-nest/BookNestPerformanceMode.tsx
+++ src/components/book-nest/BookNestPerformanceMode.tsx
@@ -1,7 +1,5 @@
 import { useEffect } from 'react'
 
-const SCROLL_SETTLE_MS = 180
-
 export function BookNestPerformanceMode() {
   useEffect(() => {
     const body = document.body
@@ -15,38 +13,7 @@ export function BookNestPerformanceMode() {
 
     body.dataset.performance = lowPowerDevice ? 'reduced' : 'standard'
 
-    let scrollTimer = 0
-    let animationFrame = 0
-
-    function clearScrolling() {
-      body.classList.remove('is-scrolling')
-    }
-
-    function handleScroll() {
-      if (!animationFrame) {
-        animationFrame = window.requestAnimationFrame(() => {
-          body.classList.add('is-scrolling')
-          animationFrame = 0
-        })
-      }
-
-      window.clearTimeout(scrollTimer)
-      scrollTimer = window.setTimeout(clearScrolling, SCROLL_SETTLE_MS)
-    }
-
-    window.addEventListener('scroll', handleScroll, { passive: true })
-    window.addEventListener('wheel', handleScroll, { passive: true })
-    window.addEventListener('touchmove', handleScroll, { passive: true })
-
     return () => {
-      window.removeEventListener('scroll', handleScroll)
-      window.removeEventListener('wheel', handleScroll)
-      window.removeEventListener('touchmove', handleScroll)
-      window.clearTimeout(scrollTimer)
-      if (animationFrame) {
-        window.cancelAnimationFrame(animationFrame)
-      }
-      body.classList.remove('is-scrolling')
       delete body.dataset.performance
     }
   }, [])
